d409fc4a0b2f37edb0fa2ed5954ac5ab6d8a18b6
APIVerve Team <hello@apiverve.com>
2025-02-03 16:35:09 -0600

    Minor Version Update

---
+++ src/apiverve_grammarcheck/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_grammarcheck',
+    version='1.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Grammar Check is a simple tool for checking the grammar and spelling of a text. It returns the corrected text.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
