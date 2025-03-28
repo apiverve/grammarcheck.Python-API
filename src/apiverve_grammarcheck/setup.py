95316b72e43fca425f9ac687294b7e5fcdc617a3
APIVerve Team <hello@apiverve.com>
2025-03-28 17:22:38 -0500

    Bug Fixes

--- src/apiverve_grammarcheck/setup.py
+++ src/apiverve_grammarcheck/setup.py
@@ -10,7 +10,7 @@ if os.path.exists(requirements_file):
 
 setup(
     name='apiverve_grammarcheck',
-    version='1.1.7',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
